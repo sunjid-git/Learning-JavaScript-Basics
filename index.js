48f39955ea500000fe7782e5db157bfd5ed49cef
sunjid-git <sunjid.info@gmail.com>
2021-01-07 19:44:47 +0600

    added

---
+++ index.js
@@ -0,0 +1,19 @@
+
+//creating a function
+function square(){
+
+    var num=5;
+    var result=num*num;
+    document.write("Result = " +result + "<br>");
+}
+//function calling
+square();
+
+
+// var num=6;
+// var result=num*num;
+// document.write("Result = " +result + "<br>");
+
+// var num=7;
+// var result=num*num;
+// document.write("Result = " +result + "<br>");
