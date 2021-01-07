56782d07495094644246fbc1b77883f5879f3ae5
sunjid-git <sunjid.info@gmail.com>
2021-01-07 20:40:58 +0600

    function

--- index.js
+++ index.js
@@ -1,15 +1,68 @@
-
-//creating a function
-function square(){
-
-    var num=5;
-    var result=num*num;
-    document.write("Result = " +result + "<br>");
+function calculator(num1,num2){
+    var num1;
+    var num2;
+    var div=num1/num2;
+    var mul=num1*num2;
+    var add=num1+num2;
+    var sub=num1-num2;
+ 
+    document.write("div : "+div+"<br>");
+    document.write("mul : "+mul+"<br>");
+    document.write("add : "+add+"<br>");
+    document.write("sub : "+sub+"<br>");
 }
-//function calling
-square();
+calculator(5,5)
 
 
+
+// // add function
+// function addition(num1, num2){
+//     var result=num1+num2;
+//     document.write("sum : "+result+"<br>");
+// }
+
+// function subtraction(num1, num2){
+//     var result=num1-num2;
+//     document.write("sub : "+result+"<br>");
+// }
+
+// addition(10,20);
+// subtraction(20,3);
+
+
+
+
+
+// //creating a function
+// function square(num1,num2){
+
+//     var result=num1*num2;
+//     return result;
+// }
+// //function calling
+// var x = square(6,6)
+// document.write(x);
+
+
+
+// //creating a function
+// function square(num){
+
+//     var result=num*num;
+//     document.write("Result = " +result + "<br>");
+// }
+// //function calling
+// square(56);
+
+// //creating a function
+// function square(){
+
+//     var num=5;
+//     var result=num*num;
+//     document.write("Result = " +result + "<br>");
+// }
+// //function calling
+// square();
 // var num=6;
 // var result=num*num;
 // document.write("Result = " +result + "<br>");
