933b1d3745dff8f0997fd8158fc1ce8c09ebe7a0
sunjid-git <sunjid.info@gmail.com>
2021-01-07 21:20:19 +0600

    array starting

--- index.js
+++ index.js
@@ -1,17 +1,81 @@
-function calculator(num1,num2){
-    var num1;
-    var num2;
-    var div=num1/num2;
-    var mul=num1*num2;
-    var add=num1+num2;
-    var sub=num1-num2;
+
+
+
+var coutry1 = ["BD", "india", "cna","usa","uk","rus"];
+var coutry2 = ["br", "sd", "vf","sds","sd","ngbh"];
+
+var x = coutry1.concat(coutry2);
+console.log(x);
+
+
+
+
+
+
+
+
+
+
+
+// var num = ["a","b","c","d","e"];
+
+
+
+// console.log(num);
+// console.log(num.length);
+// num.push("sokhina");
+// num.push("fokhina");
+// num.push("tokhina");
+// num.push("rokhina");
+// console.log(num);
+// console.log(num.length);
+
+// num.pop();
+// console.log(num);
+// console.log(num.length);
+
+// var names=new Array(5);
+// names[0] ="Anis"; 
+// names[1] ="Amin"; 
+// names[2] ="Biman"; 
+// names[3] ="Bima"; 
+// // names[4] ="tafi"; 
+// console.log(num[4]);
+// console.log(names);
+// console.log(names[2]);
+// console.log(names[4]);
+// console.log(names[5]);
+
+
+// var name1, name2, name3, name4, name5;
+// name1 ="Sunjid";
+// name2 ="Hasan";
+// name3 ="Mafi";
+// name4 ="Tashrif";
+// name5 ="Tarannum";
+
+// console.log(name1);
+
+
+
+
+
+
+
+// function calculator(num1,num2){
+//     var num1;
+//     var num2;
+//     var div=num1/num2;
+//     var mul=num1*num2;
+//     var add=num1+num2;
+//     var sub=num1-num2;
  
-    document.write("div : "+div+"<br>");
-    document.write("mul : "+mul+"<br>");
-    document.write("add : "+add+"<br>");
-    document.write("sub : "+sub+"<br>");
-}
-calculator(5,5)
+//     document.write("div : "+div+"<br>");
+//     document.write("mul : "+mul+"<br>");
+//     document.write("add : "+add+"<br>");
+//     document.write("sub : "+sub+"<br>");
+// }
+// calculator(5,5)
 
 
 
