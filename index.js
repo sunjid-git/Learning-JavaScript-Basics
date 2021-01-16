f9e40f355ab7ef8b4f441b10bfccdcd041304668
sunjid-git <sunjid.info@gmail.com>
2021-01-16 19:59:09 +0600

    loop in array

--- index.js
+++ index.js
@@ -1,136 +1,21 @@
 
+var num = [10,20,30,40,50];
+var sum =0;
 
+for(var i=0; i<5; i++){
+    console.log(num[i]);
+    sum=sum +num[i];
+}
 
-var coutry1 = ["BD", "india", "cna","usa","uk","rus"];
-var coutry2 = ["br", "sd", "vf","sds","sd","ngbh"];
-
-var x = coutry1.concat(coutry2);
-console.log(x);
-
-
-
-
-
-
-
-
-
-
-
-// var num = ["a","b","c","d","e"];
-
-
-
-// console.log(num);
-// console.log(num.length);
-// num.push("sokhina");
-// num.push("fokhina");
-// num.push("tokhina");
-// num.push("rokhina");
-// console.log(num);
-// console.log(num.length);
-
-// num.pop();
-// console.log(num);
-// console.log(num.length);
-
-// var names=new Array(5);
-// names[0] ="Anis"; 
-// names[1] ="Amin"; 
-// names[2] ="Biman"; 
-// names[3] ="Bima"; 
-// // names[4] ="tafi"; 
-// console.log(num[4]);
-// console.log(names);
-// console.log(names[2]);
-// console.log(names[4]);
-// console.log(names[5]);
-
-
-// var name1, name2, name3, name4, name5;
-// name1 ="Sunjid";
-// name2 ="Hasan";
-// name3 ="Mafi";
-// name4 ="Tashrif";
-// name5 ="Tarannum";
-
-// console.log(name1);
-
-
-
-
-
-
-
-// function calculator(num1,num2){
-//     var num1;
-//     var num2;
-//     var div=num1/num2;
-//     var mul=num1*num2;
-//     var add=num1+num2;
-//     var sub=num1-num2;
+console.log("sum : "+sum);
  
-//     document.write("div : "+div+"<br>");
-//     document.write("mul : "+mul+"<br>");
-//     document.write("add : "+add+"<br>");
-//     document.write("sub : "+sub+"<br>");
-// }
-// calculator(5,5)
-
-
-
-// // add function
-// function addition(num1, num2){
-//     var result=num1+num2;
-//     document.write("sum : "+result+"<br>");
-// }
-
-// function subtraction(num1, num2){
-//     var result=num1-num2;
-//     document.write("sub : "+result+"<br>");
-// }
-
-// addition(10,20);
-// subtraction(20,3);
 
 
 
 
 
-// //creating a function
-// function square(num1,num2){
 
-//     var result=num1*num2;
-//     return result;
-// }
-// //function calling
-// var x = square(6,6)
-// document.write(x);
-
-
-
-// //creating a function
-// function square(num){
-
-//     var result=num*num;
-//     document.write("Result = " +result + "<br>");
-// }
-// //function calling
-// square(56);
-
-// //creating a function
-// function square(){
-
-//     var num=5;
-//     var result=num*num;
-//     document.write("Result = " +result + "<br>");
-// }
-// //function calling
-// square();
-// var num=6;
-// var result=num*num;
-// document.write("Result = " +result + "<br>");
-
-// var num=7;
-// var result=num*num;
-// document.write("Result = " +result + "<br>");
+// console.log(num[0]);
+// console.log(num[1]);
+// console.log(num[2]);
+// console.log(num[3]);
